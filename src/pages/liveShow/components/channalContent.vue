6f144ea8d8afcbd4b54f5af73b9c50f6a389d3cb
xiaoguaishou <1562491176@qq.com>
2018-11-14 09:08:01 +0800

    yth2

--- src/pages/liveShow/components/channalContent.vue
+++ src/pages/liveShow/components/channalContent.vue
@@ -30,20 +30,27 @@ export default {
             width: 340px;
             height: 100%;
             .each{
+                position: relative;
                 float: left;
                 width: 148px;
                 height: 178px;
-                margin-right: 22px;
+                padding-right: 22px;
                 font-size: 16px;
                 color: #545454;
                 text-align: center;
+                /* box-sizing: border-box */
                 .sanjiao{
-                    /* position: absolute; */
                     margin-left: 18px;
                     margin-top: -165px;
                 }
                 .course{
-                    margin-top: -16px;
+                    position: absolute;
+                    left  0
+                    bottom 16px
+                    letter-spacing 1px
+                    width 100%
+                    text-align center
+                    line-height 22px
                 }
             }
         }
